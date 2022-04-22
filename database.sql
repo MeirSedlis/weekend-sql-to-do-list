4c06acdd3bca478ba79bf0af8520f1a2de1cadb4
Meir Sedlis <meirsedlis@gmail.com>
2022-04-22 16:01:30 -0500

    add database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "tasks"(
+"id" SERIAL PRIMARY KEY,
+"task" VARCHAR (255) NOT NULL,
+"dueDate" DATE,
+"importance" INT CHECK ("importance"<=5 AND "importance" >0),
+"description" VARCHAR (500),
+"complete" BOOLEAN
+);
+
+SELECT * FROM "tasks";
